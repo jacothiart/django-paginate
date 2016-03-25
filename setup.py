608dd993cce8524830d5ee4d64b4ac68636ed879
Jaco Thiart <jaco.thiart.android@gmail.com>
2016-03-25 14:49:51 +0200

    django paginate ppackage manager build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-paginate',
+      version='0.3.1',
+      description='Django Paginate',
+      url='https://github.com/jacothiart/django-paginate',
+      author='Jaco Thiart',
+      author_email='jaco@jacothiart.com',
+      license='MIT',
+      packages=['paginate'],
+      zip_safe=False)
